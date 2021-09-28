9674ba7ce9af8de4e6213ad74b303537e0be2fbe
creator-krixus <wilsonrueda2@gmail.com>
2021-09-27 21:43:10 -0500

    Centrado del icono whats app

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,8 +7,8 @@ export default function Footer() {
     return (
         <div className="footer">
             <p>Barbosa Antioquia</p>
-            <p>Click sobre el icono de whats up para contactarme</p>
-            <a href="https://api.whatsapp.com/send?phone=+573114191234&text=¡Hola estoy interesado en alquilar la finca!"><img src={whast} width="100px" alt="whats up"/></a>
+            <p>Click sobre el icono de whats app para contactarme</p>
+            <a href="https://api.whatsapp.com/send?phone=+573114191234&text=¡Hola estoy interesado en alquilar la finca!"><img src={whast} width="100px" alt="whats app" className="whats__app"/></a>
         </div>
     )
 }
